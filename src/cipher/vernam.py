fafe32a669bd76cc478854ddbda31b0dbf6a235f
vladqstrn <vldqq@outlook.com>
2021-09-29 22:38:02 +0300

    added class VernamDescriptor

--- src/cipher/vernam.py
+++ src/cipher/vernam.py
@@ -23,23 +23,45 @@ class VernamEncrypter:
         key = self.chr_to_binary(key)
         return key
 
-    def encryption(self, message, key):
+    def encryption(self, message, key) -> list:
         """Сложение по модулю 2"""
         buff = []
         for i in range(len(message)):
             buff.append(int(message[i], 2) ^ int(key[i], 2))
         return buff
 
-    def sum(self, a):
+    def sum(self, a) -> list:
         """Вывод двоичного шифрованного сообщения"""
         sum = []
         for i in range(len(a)):
             sum.append('{0:0b}'.format(a[i]))
         return sum
 
-    def encripted_message(self, a):
+    def encripted_message(self, a) -> str:
         """Вывод шифрованного сообщения в формате ascii"""
         buff = []
         for x in range(len(a)):
             buff.append(chr(a[x]))
         return ''.join(buff)
+
+
+class VernamDescriptor(VernamEncrypter):
+    """
+    Класс для расшифрования сообщения
+    """
+    def __init__(self, cript_message):
+        self.cript_message = cript_message
+
+    def convert_ascii_to_binary(self) -> list:
+        """"Перевод из формата ascii в двоичный код"""
+        buff = []
+        for i in range(len(self.cript_message)):
+            buff.append(bin(ord(self.cript_message[i])))
+        return buff
+
+    def return_decrypted_message(self, binary_message) -> str:
+        """Возвращает расшифрованное сообщение"""
+        decrypted_buff = []
+        for i in range(len(binary_message)):
+            decrypted_buff.append(chr(binary_message[i]))
+        return ''.join(decrypted_buff)
