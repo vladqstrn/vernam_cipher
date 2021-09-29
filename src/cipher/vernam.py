70d6c0596dddbaa29c3c3055754d49933460c72d
vladqstrn <vldqq@outlook.com>
2021-09-29 22:18:34 +0300

    code refactoring

--- src/cipher/vernam.py
+++ src/cipher/vernam.py
@@ -3,44 +3,42 @@ import string
 
 
 class VernamEncrypter:
-    ''''Класс для шифрования методом Вернама'''
+    """Класс для шифрования методом Вернама"""
 
     def __init__(self, message, key_len):
         self.message = message
         self.key_len = key_len
-        pass
 
-    def chr_to_binary(self,message) -> list:
-        "Перевод сообщения в двоичную систему счисления"
-        binary_list =[]
+    def chr_to_binary(self, message) -> list:
+        """Перевод сообщения в двоичную систему счисления"""
+        binary_list = []
         for x in message:
             binary_list.append(bin(ord(x)))
         return binary_list
 
     def generate_key(self) -> list:
-        "Создание случайного ключа"
+        """Создание случайного ключа"""
         alphabet = string.ascii_lowercase + string.ascii_uppercase
         key = ''.join(random.choice(alphabet) for _ in range(self.key_len))
         key = self.chr_to_binary(key)
         return key
 
     def encryption(self, message, key):
-        "Сложение по модулю 2"
+        """Сложение по модулю 2"""
         buff = []
         for i in range(len(message)):
-            buff.append(int(message[i],2) ^ int(key[i],2))
+            buff.append(int(message[i], 2) ^ int(key[i], 2))
         return buff
-        
-        
+
     def sum(self, a):
-        "Вывод доичного шифрованного сообщения"
+        """Вывод двоичного шифрованного сообщения"""
         sum = []
         for i in range(len(a)):
             sum.append('{0:0b}'.format(a[i]))
         return sum
-    
+
     def encripted_message(self, a):
-        "Вывод шифрованного сообщения в формате ascii"
+        """Вывод шифрованного сообщения в формате ascii"""
         buff = []
         for x in range(len(a)):
             buff.append(chr(a[x]))
