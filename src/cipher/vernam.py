92a0d4e5f2613d8cfed3328a85e40099b2ac99a3
vladqstrn <vldqq@outlook.com>
2021-10-11 17:22:21 +0300

    code refactoring vernamEncripter

--- src/cipher/vernam.py
+++ src/cipher/vernam.py
@@ -5,44 +5,52 @@ import string
 class VernamEncrypter:
     """Класс для шифрования методом Вернама"""
 
-    def __init__(self, message, key_len):
+    def __init__(self, message):
         self.message = message
-        self.key_len = key_len
 
-    def chr_to_binary(self, message) -> list:
+    def _chr_to_binary(self, data) -> list:
         """Перевод сообщения в двоичную систему счисления"""
         binary_list = []
-        for x in message:
+        for x in data:
             binary_list.append(bin(ord(x)))
         return binary_list
 
-    def generate_key(self) -> list:
+    def _generate_key(self) -> list:
         """Создание случайного ключа"""
         alphabet = string.ascii_lowercase + string.ascii_uppercase
-        key = ''.join(random.choice(alphabet) for _ in range(self.key_len))
-        key = self.chr_to_binary(key)
-        return key
+        key = ''.join(random.choice(alphabet) for _ in range(len(self.message)))
+        key_binary = self._chr_to_binary(key)
+        return key, key_binary
 
-    def encryption(self, message, key) -> list:
+    def _encryption(self, message, key) -> list:
         """Сложение по модулю 2"""
         buff = []
         for i in range(len(message)):
             buff.append(int(message[i], 2) ^ int(key[i], 2))
         return buff
 
-    def sum(self, a) -> list:
+    def _sum(self, data) -> list:
         """Вывод двоичного шифрованного сообщения"""
         sum = []
-        for i in range(len(a)):
-            sum.append('{0:0b}'.format(a[i]))
+        for i in range(len(data)):
+            sum.append('{0:0b}'.format(data[i]))
         return sum
 
-    def encripted_message(self, a) -> str:
+    def _encripted_message(self, data) -> str:
         """Вывод шифрованного сообщения в формате ascii"""
         buff = []
-        for x in range(len(a)):
-            buff.append(chr(a[x]))
+        for x in range(len(data)):
+            buff.append(chr(data[x]))
         return ''.join(buff)
+    
+    def process_enryption(self) -> list:
+        binary_message = self._chr_to_binary(self.message)
+        key, key_binary = self._generate_key()
+        enc = self._encryption(binary_message ,key_binary)
+        log_enc = self._sum(enc)
+        log_enc_ascii = self._encripted_message(enc)
+        return enc, key, key_binary, log_enc, log_enc_ascii
+        
 
 
 class VernamDescriptor(VernamEncrypter):
@@ -51,6 +59,7 @@ class VernamDescriptor(VernamEncrypter):
     """
     def __init__(self, cript_message):
         self.cript_message = cript_message
+        
 
     def convert_ascii_to_binary(self) -> list:
         """"Перевод из формата ascii в двоичный код"""
@@ -65,3 +74,23 @@ class VernamDescriptor(VernamEncrypter):
         for i in range(len(binary_message)):
             decrypted_buff.append(chr(binary_message[i]))
         return ''.join(decrypted_buff)
+
+test = VernamEncrypter('hello')
+#print(test.process_enryption())
+# test = VernamEncrypter('he', 11)
+# binary_message = test.chr_to_binary('hello world')
+# binary_key = test.generate_key()
+# print('message - ',binary_message)
+# print('key - ', binary_key)
+# a = test.encryption(binary_message, binary_key)
+# print('this is xor-----------',a)
+# b = (test.sum(a))
+# print('this is byte xor---------', b)
+# cript = (test.encripted_message(a))
+# print('this is cripr', cript)
+
+# f = VernamDescriptor(cript)
+# d = f.convert_ascii_to_binary()
+# print('this is binary',d)
+# ff = test.encryption(d,binary_key)
+# print(f.return_decrypted_message(ff))
