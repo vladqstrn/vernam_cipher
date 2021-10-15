341efd5ad38cf020e4b1a4ff281e0672f770f183
vladqstrn <vldqq@outlook.com>
2021-10-15 23:50:30 +0300

    code refactoring

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,21 @@
-from cipher import vernam
-m = input()
-a = vernam.VernamEncrypter()
-# class Controller:
-#     def run():
+import console_view
+
+
+class Controller:
+    def __init__(self, view, encrypter, descriptor):
+        self.view = view
+        self.encrypter = encrypter
+        self.descriptor = descriptor
+
+    def run(self):
+        self.view.show_menu()
+        self.menu_choice()
+
+    def menu_choice(self):
+        self.view._ask_choice_menu()
+        choice = int(input())
         
+        if choice == 1:
+            self.view._ask_message()
+            message = input()
+            print(self.encrypter.process_enryption(message))
