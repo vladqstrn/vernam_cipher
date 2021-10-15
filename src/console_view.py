00fbb2a6e262c4ddb5cd8bc0d4aa0071df8bec46
vladqstrn <vldqq@outlook.com>
2021-10-15 23:47:15 +0300

    added rendering methods

--- src/console_view.py
+++ src/console_view.py
@@ -2,20 +2,24 @@ class ConsoleView:
     def __init__(self):
         self.menu_items = [
             "Зашифровать сообщение",
-            "Расшифровать сообщение"
+            "Расшифровать сообщение",
             "Выход"
         ]
 
     def _generate_menu(self):
         """
-        Создаёт меню из элементов menu_item
+        Создаёт меню из элементов menu_items
         """
         return "\n" + "".join([str(index + 1) + ". " + item + "\n" for index, item in enumerate(self.menu_items)])
+
+    def _ask_choice_menu(self):
+        print('Выберите вариант: ')
     
-    def menu_choice(self):
-        choice = int(input("Выберите вариант: "))
-        return choice
-    
+    def _ask_message(self):
+        print('Введите сообщение: ')
+        
+
     def show_menu(self):
         menu = self._generate_menu()
         print(menu)
+
