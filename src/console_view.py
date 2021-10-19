24449a7225524c831cd2652bfffe5582279a4115
vladqstrn <vldqq@outlook.com>
2021-10-19 14:30:16 +0300

    added console menu for encription/description

--- src/console_view.py
+++ src/console_view.py
@@ -22,4 +22,25 @@ class ConsoleView:
     def show_menu(self):
         menu = self._generate_menu()
         print(menu)
+    
+    def _ask_log(self):
+        print('Показать лог?\n1 - Да\n0 - Нет')
+    
+    def _print_log(self,  binary_message, xor, binary_key, binary_encripted_message):
+        print(binary_message, 'Двоичное сообщение\n',\
+            binary_key, 'Двоичный ключ\n',\
+            binary_encripted_message, 'Зашифрованное двоичное сообщение\n',
+            xor, 'XOR'
+            )
+    
+    def _encription_log(self, key, ascii_message):
+        print(
+            key, ' - Ключ\n',\
+            ascii_message, 'Зашифрованное сообщение\n'
+            )
+    
+    def _start_description_menu(self):
+        print('Введите сообщение\nВведите ключ')
 
+    def _return_descripted_message(self, message):
+        print(message, '- сообщение')
